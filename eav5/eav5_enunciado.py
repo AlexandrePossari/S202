9a6a0db7e9b48a950bedc8445781006b05c03ec8
Alexandre Possari <ac.possari@gmail.com>
2024-06-10 20:54:33 -0300

    feat: questão1 in eav5

--- eav5/eav5_enunciado.py
+++ eav5/eav5_enunciado.py
@@ -34,9 +34,10 @@ Imagine que será mantido também um lista de posts já vistos por um determinad
 import redis
 
 redis_conn = redis.Redis(
-    host="...", port=0,
+    host="localhost", 
+    port=6379,
     username="default", # use your Redis user. More info https://redis.io/docs/latest/operate/oss_and_stack/management/security/acl/
-    password="...", # use your Redis password
+    password="my-password", # use your Redis password
     decode_responses=True
 )
 
@@ -44,37 +45,23 @@ redis_conn = redis.Redis(
 
 # Questão 1
 def questao_1(users):
-    redis_conn.hset("user:1", mapping={
-        "nome": "Serafim Amarantes",
-        "email": "samarantes@g.com"
-    })
-    redis_conn.hset("user:2", mapping={
-        "nome": "Tamara Borges",
-        "email": "tam_borges@g.com"
-    })
-    redis_conn.hset("user:3", mapping={
-        "nome": "Ubiratã Carvalho",
-        "email": "bira@g.com"
-    })
-    redis_conn.hset("user:4", mapping={
-        "nome": "Valéria Damasco",
-        "email": "valeria_damasco@g.com"
-    })
-
-    redis_conn.lpush("users", "user:1","user:2","user:3","user:4")
-
-    numero_usuarios = redis_conn.llen("users")
-
-    users = redis_conn.lrange("users", 0, numero_usuarios-1)
-    print(users)
     for user in users:
-        print(redis_conn.hget(user, "nome"))
-        print(redis_conn.hget(user, "email"))
-        print()
+        nome = user["nome"]
+        email = user["email"]        
+        id = user["id"]        
+        redis_conn.hset(f"user:{id}", mapping={
+            "id": id,
+            "nome": nome,
+            "email": email
+        })
+
+    final_list = []
+    for user in users:
+        final_list.append(redis_conn.hgetall(f"user:{user["id"]}"))
+    print(final_list)
     pass
-    return users
-
-
+    
+    return final_list
 
 def test_questao_1():
 
@@ -84,19 +71,17 @@ def test_questao_1():
         {"id":'3', "nome":"Ubiratã Carvalho", "email":"bira@g.com"},
         {"id":'4', "nome":"Valéria Damasco", "email":"valeria_damasco@g.com"}
     ]
-
     assert users == sorted(questao_1(users), key=lambda d: d['id'])
-
 
 # Questão 2
 def questao_2(interests):
-    redis_conn.lpush("interests1", {"futebol":0.855}, {"pagode":0.765}, {"engraçado":0.732}, {"cerveja":0.622}, {"estética":0.519})
-    redis_conn.lpush("interests2", {"estética":0.765}, {"jiujitsu":0.921}, {"luta":0.884}, {"academia":0.541}, {"maquiagem":0.658})
-    redis_conn.lpush("interests3", {"tecnologia":0.999}, {"hardware":0.865}, {"games":0.745}, {"culinária":0.658}, {"servers":0.54})
-    redis_conn.lpush("interests4", {"neurociências":0.865}, {"comportamento":0.844}, {"skinner":0.854}, {"laboratório":0.354}, {"pesquisa":0.428})
+    id = 1
+    for interest in interests:
+        redis_conn.lpush(f"interests{id}", interest["interesses"])
 
     n_interests = redis_conn.llen("interests1")
     interests1 = redis_conn.lrange("interests1", 0, n_interests-1)
+    print(interests1)
     n_interests = redis_conn.llen("interests2")
     interests2 = redis_conn.lrange("interests2", 0, n_interests-1)
     n_interests = redis_conn.llen("interests3")
@@ -120,63 +105,63 @@ def test_questao_2():
     assert output == questao_2(interests)
 
 
-# Questão 3
-def questao_3(posts):
-    pass
+# # Questão 3
+# def questao_3(posts):
+#     pass
 
-def test_questao_3():
+# def test_questao_3():
 
-    posts = [
-        {"id": '345', "autor":"news_fc@g.com", "data_hora": "2024-06-10 19:51:03", "conteudo": "Se liga nessa lista de jogadores que vão mudar de time no próximo mês!", "palavras_chave": "brasileirao, futebol, cartola, esporte" },
-        {"id": '348', "autor":"gastro_pub@g.com", "data_hora": "2024-06-10 19:55:13", "conteudo": "Aprenda uma receita rápida de onion rings super crocantes.", "palavras_chave": "onion rings, receita, gastronomia, cerveja, culinária" },
-        {"id": '349', "autor":"make_with_tina@g.com", "data_hora": "2024-06-10 19:56:44", "conteudo": "A dica de hoje envolve os novos delineadores da linha Rare Beauty", "palavras_chave": "maquiagem, estética, beleza, delineador" },
-        {"id": '350', "autor":"samarantes@g.com", "data_hora": "2024-06-10 19:56:48", "conteudo": "Eu quando acho a chuteira que perdi na última pelada...", "palavras_chave": "pelada, futebol, cerveja, parceiros" },
-        {"id": '351', "autor":"portal9@g.com", "data_hora": "2024-06-10 19:57:02", "conteudo": "No último mês pesquisadores testaram três novos medicamentos para ajudar aumentar o foco.", "palavras_chave": "neurociências, tecnologia, foco, medicamento" },
-        {"id": '352', "autor":"meme_e_cia@g.com", "data_hora": "2024-06-10 19:58:33", "conteudo": "Você prefere compartilhar a nossa página agora ou daqui cinco minutos?", "palavras_chave": "entretenimento, engraçado, viral, meme" },
-        {"id": '353', "autor":"rnd_hub@g.com", "data_hora": "2024-06-10 19:59:59", "conteudo": "A polêmica pesquisa de V. Damasco sobre ciência do comportamente acaba de ser publicada.", "palavras_chave": "comportamento, ciência, pesquisa, damasco" }
-    ]
+#     posts = [
+#         {"id": '345', "autor":"news_fc@g.com", "data_hora": "2024-06-10 19:51:03", "conteudo": "Se liga nessa lista de jogadores que vão mudar de time no próximo mês!", "palavras_chave": "brasileirao, futebol, cartola, esporte" },
+#         {"id": '348', "autor":"gastro_pub@g.com", "data_hora": "2024-06-10 19:55:13", "conteudo": "Aprenda uma receita rápida de onion rings super crocantes.", "palavras_chave": "onion rings, receita, gastronomia, cerveja, culinária" },
+#         {"id": '349', "autor":"make_with_tina@g.com", "data_hora": "2024-06-10 19:56:44", "conteudo": "A dica de hoje envolve os novos delineadores da linha Rare Beauty", "palavras_chave": "maquiagem, estética, beleza, delineador" },
+#         {"id": '350', "autor":"samarantes@g.com", "data_hora": "2024-06-10 19:56:48", "conteudo": "Eu quando acho a chuteira que perdi na última pelada...", "palavras_chave": "pelada, futebol, cerveja, parceiros" },
+#         {"id": '351', "autor":"portal9@g.com", "data_hora": "2024-06-10 19:57:02", "conteudo": "No último mês pesquisadores testaram três novos medicamentos para ajudar aumentar o foco.", "palavras_chave": "neurociências, tecnologia, foco, medicamento" },
+#         {"id": '352', "autor":"meme_e_cia@g.com", "data_hora": "2024-06-10 19:58:33", "conteudo": "Você prefere compartilhar a nossa página agora ou daqui cinco minutos?", "palavras_chave": "entretenimento, engraçado, viral, meme" },
+#         {"id": '353', "autor":"rnd_hub@g.com", "data_hora": "2024-06-10 19:59:59", "conteudo": "A polêmica pesquisa de V. Damasco sobre ciência do comportamente acaba de ser publicada.", "palavras_chave": "comportamento, ciência, pesquisa, damasco" }
+#     ]
 
-    assert posts == sorted(questao_3(posts), key=lambda d: d['id'])
+#     assert posts == sorted(questao_3(posts), key=lambda d: d['id'])
 
 
-# Questão 4
-def questao_4(user_id):
-    pass
+# # Questão 4
+# def questao_4(user_id):
+#     pass
 
-def test_questao_4():
+# def test_questao_4():
 
-    user_id = 3
+#     user_id = 3
 
-    output = [
-        "No último mês pesquisadores testaram três novos medicamentos para ajudar aumentar o foco.",
-        "Aprenda uma receita rápida de onion rings supoer crocantes.",
-        "Se liga nessa lista de jogadores que vão mudar de time no próximo mês!",
-        "A dica de hoje envolve os novos delineadores da linha Rare Beauty",
-        "Eu quando acho a chuteira que perdi na última pelada...",
-        "Você prefere compartilhar a nossa página agora ou daqui cinco minutos?",
-        "A polêmica pesquisa de V. Damasco sobre ciência do comportamente acaba de ser publicada."               
-    ]
+#     output = [
+#         "No último mês pesquisadores testaram três novos medicamentos para ajudar aumentar o foco.",
+#         "Aprenda uma receita rápida de onion rings supoer crocantes.",
+#         "Se liga nessa lista de jogadores que vão mudar de time no próximo mês!",
+#         "A dica de hoje envolve os novos delineadores da linha Rare Beauty",
+#         "Eu quando acho a chuteira que perdi na última pelada...",
+#         "Você prefere compartilhar a nossa página agora ou daqui cinco minutos?",
+#         "A polêmica pesquisa de V. Damasco sobre ciência do comportamente acaba de ser publicada."               
+#     ]
 
-    assert output == questao_4(user_id)
+#     assert output == questao_4(user_id)
 
-# Questão 5
-def questao_5(user_views, user_id):
-    pass
+# # Questão 5
+# def questao_5(user_views, user_id):
+#     pass
 
-def test_questao_5():
+# def test_questao_5():
 
-    user_id = 3
-    user_views = [
-        {"usuario":1, "visualizado": [345,350,353]},
-        {"usuario":2, "visualizado": [350,351]},
-        {"usuario":3, "visualizado": [345,351,352,353]},
-        {"usuario":4, "visualizado": []}
-    ]
+#     user_id = 3
+#     user_views = [
+#         {"usuario":1, "visualizado": [345,350,353]},
+#         {"usuario":2, "visualizado": [350,351]},
+#         {"usuario":3, "visualizado": [345,351,352,353]},
+#         {"usuario":4, "visualizado": []}
+#     ]
 
-    output = [
-        "Aprenda uma receita rápida de onion rings supoer crocantes.",
-        "A dica de hoje envolve os novos delineadores da linha Rare Beauty",
-        "Eu quando acho a chuteira que perdi na última pelada..."   
-    ]
+#     output = [
+#         "Aprenda uma receita rápida de onion rings supoer crocantes.",
+#         "A dica de hoje envolve os novos delineadores da linha Rare Beauty",
+#         "Eu quando acho a chuteira que perdi na última pelada..."   
+#     ]
 
-    assert output == questao_5(user_views, user_id)
+#     assert output == questao_5(user_views, user_id)
